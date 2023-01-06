59bfb167f190f6410ed140103c3ef79d40a5f475
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-01-06 20:06:38 +0900

    Impl extension main logic

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+(() => {
+  const dialog = (url: string) => {
+    const anchor = `<a href="${url}">${url}</a>`;
+    const elem = document.createElement("div");
+    elem.innerHTML = `Redirect to <pre style="display: inline-block">${anchor}</pre> after 3 seconds.`;
+    elem.setAttribute("style", "font-size: 20px; font-weight: bold");
+    return elem;
+  };
+
+  const location = document.location.pathname;
+
+  if (
+    location.startsWith("/cgi-bin/scat.rb/ruby/ruby-dev/") ||
+    location.startsWith("/cgi-bin/scat.rb/ruby/ruby-core/")
+  ) {
+    const newPath = location.replace("/cgi-bin/scat.rb/ruby/", "");
+    const newUrl = `https://blade.ruby-lang.org/${newPath}`;
+    const htmlBody = document.getElementsByTagName("body")[0];
+    setTimeout(() => {
+      htmlBody.prepend(dialog(newUrl));
+    }, 500);
+
+    setTimeout(() => {
+      // window.location.assign(newUrl);
+    }, 3500);
+  }
+})();
